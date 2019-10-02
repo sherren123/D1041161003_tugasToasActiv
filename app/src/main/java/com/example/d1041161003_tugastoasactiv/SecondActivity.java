918d09acb3a6b3216183dd969d2ec48a61a1a500
sherrenja123 <sherrenaling@gmail.com>
2019-10-02 20:27:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/d1041161003_tugastoasactiv/SecondActivity.java
@@ -0,0 +1,38 @@
+package com.example.d1041161003_tugastoasactiv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity  extends AppCompatActivity {
+
+    private int mCount = 03;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mShowCount = findViewById(R.id.show_count);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_nama);
+        textView.setText("Hallo " + message);
+    }
+
+    public void countUp(View view) {
+        mCount++ ;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void countDown(View view) {
+        mCount-- ;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+}
